046cbe6f53987d46181281ce520d3aa93c35b49f
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-03-15 14:31:27 +0100

    next lection upload

--- server/server.js
+++ server/server.js
@@ -1,2 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+const myDataBase = "myDB";
+const url = `mongodb://localhost:27019/${myDataBase}`;
+mongoose.connect(url)
+    .then(() => console.log("connected to MongoDB"))
+    .catch(err => console.log("connection error:", err));
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+});
+
+const User = mongoose.model("User", myDataBase);
+ 
+app.get("/api/users", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.json({})
+    } catch {
+        res.status(500).json({message: err.message})
+    }
+})
+ 
+app.listen(PORT, () => console.log("Server express is running", PORT));
+ 
+process.on("SIGINT", () => {
+    console.log("Closing MongoDB")
+    mongoose.disconnect()
+        .then(()=>console.log("MongoDB connection closed"))
+        .finally(()=>process.exit())
+});
