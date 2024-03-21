bb2ce2a0a43851a8f67d4968eff363c2288a1ed2
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-03-21 15:23:02 +0100

    next upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,26 @@ function App() {
     consle.log("I am fetching");
     try {
       const res = await fetch("http://localhost:8000/api/users", {method: "GET"});
-      if (!res.ok) throw Error("ntework responce was not ok:", res.status);
+      if (!res.ok) throw Error(`network responce was not ok: ${res.status}`);
+
+      const data = await res.json();
+      setUserList(data);
     } catch (error) {
       console.log("Error:", error);
     }
+  }
 
-    const data = await res.json();
-    setUserList(data);
+  const deleteUser = async (userID) => {
+    const confirmation = window.confirm("Czy chcesz skasować użytkownika");
+
+    if (!confirmation) return
+
+    try {
+      const res = await fetch(`http://localhost:8000/api/users/:${userID}`, {method: "DELETE"});
+      if (!res.ok) throw new Error("Error response is not ok");
+    } catch (err) {
+      console.log()
+    }
   }
 
   return (
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(cors());
 
 const myDataBase = "myDB";
 const url = `mongodb://localhost:27019/${myDataBase}`;
+
 mongoose.connect(url)
     .then(() => console.log("connected to MongoDB"))
     .catch(err => console.log("connection error:", err));
@@ -20,14 +21,25 @@ const userSchema = mongoose.Schema({
     age: Number
 });
 
-const User = mongoose.model("User", myDataBase);
+const User = mongoose.model("User", userSchema);
  
 app.get("/api/users", async (req, res) => {
     try {
         const users = await User.find({});
-        res.json({})
+        res.json(users)
     } catch {
         res.status(500).json({message: err.message})
+    }
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+    try {
+        const userID = req.params.id;
+        const deleteUser = await User.findByIdAndDelete(userID);
+        if (!deleteUser)
+            return res.status(404).json({message: "User not found"});
+    } catch (err) {
+        res.status(500).json({message: err.message});
     }
 })
  
@@ -36,6 +48,6 @@ app.listen(PORT, () => console.log("Server express is running", PORT));
 process.on("SIGINT", () => {
     console.log("Closing MongoDB")
     mongoose.disconnect()
-        .then(()=>console.log("MongoDB connection closed"))
-        .finally(()=>process.exit())
+        .then(() => console.log("MongoDB connection closed"))
+        .finally(() => process.exit())
 });
